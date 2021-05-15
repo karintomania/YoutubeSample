92d9a52409c242c6ca4b7a1ecdb2fa67a38a1579
karintomania <19652340+karintomania@users.noreply.github.com>
2021-05-15 23:30:36 +0100

    first commit

---
+++ app/src/main/java/com/bedroomcomputing/youtubesample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.bedroomcomputing.youtubesample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val youTubePlayerView = findViewById<YouTubePlayerView>(R.id.youtube_player_view)
+        lifecycle.addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = "x8VYWazR5mE"
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
+    }
+}
